2dfbf1233d2da661297c86a80f14677091dd59f4
Malte <91071750+Loxiuz@users.noreply.github.com>
2024-06-20 15:52:22 +0200

    relevant types

--- src/api/types.ts
+++ src/api/types.ts
@@ -4,4 +4,27 @@ interface Template {
   description: string;
 }
 
-export type { Template };
+interface Participant {
+  id: number | null;
+  fullName: string;
+  email: string;
+  gender: string;
+  birthDate: string;
+  club: string;
+}
+
+interface Discipline {
+  id: number | null;
+  name: string;
+  resultType: string;
+}
+
+interface Result {
+  id: number | null;
+  participantId: number;
+  disciplineId: number;
+  date: string;
+  value: string;
+}
+
+export type { Template, Participant, Discipline, Result };
